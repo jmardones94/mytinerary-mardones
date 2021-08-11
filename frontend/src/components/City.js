2540dd4c3e60c62d8196a14b81b5a8462c1fce2e
jmardones94 <jmardonesg.94@gmail.com>
2021-08-11 12:07:24 -0400

    error en require

--- frontend/src/components/City.js
+++ frontend/src/components/City.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function City({ city }) {
     useEffect(() => {
@@ -12,6 +13,8 @@ export default function City({ city }) {
                 backgroundSize: 'cover'}}>
             </div>
             <h1 className="p-5 tracking-wide font-semibold text-xl md:text-3xl text-center uppercase dark:text-gray-200">{city.name} <span className="tracking-wider normal-case font-silt">MyTineraries</span></h1>
+            <p className="text-center text-gray-900 dark:text-gray-100 mt-5">Site under construction</p>
+            <Link className="flex w-full justify-center m-3" to="/cities"><button className="text-center text-gray-900 dark:text-gray-100 px-3 py-2 bg-red-500 rounded">Back to Cities</button></Link>
         </div>
     )
 }
--- server.js
+++ server.js
@@ -1,5 +1,35 @@
 const express = require('express')
+const cors = require('cors')
+const { request } = require('express')
+require('dotenv').config()
 
 const app = express()
+
+// Middlewares
+app.use(cors()) 
+app.use(express.json())
+
+const citiesData = [
+    {_id: 1, src: '../assets/cities/toronto.jpg', alt:'Toronto',name:'Toronto', country:'Canada'},
+    {_id: 2, src: '../assets/cities/sydney.jpg', alt:'Sydney',name:'Sydney', country:'Australia'},
+    {_id: 3, src: '../assets/cities/london.jpg',alt:'London',name:'London', country:'United Kingdom'},
+    {_id: 4, src: '../assets/cities/venice.jpg',alt:'Venice',name:'Venice', country:'Italy'},
+    {_id: 5, src: '../assets/cities/berlin.jpg',alt:'Berlin',name:'Berlin', country:'Germany'},
+    {_id: 6, src: '../assets/cities/torresdelpaine.jpg',alt:'Torres del Paine',name:'Torres del Paine', country:'Chile'},
+    {_id: 7, src: '../assets/cities/buenosaires.jpg',alt:'Buenos Aires',name:'Buenos Aires', country:'Argentina'},
+    {_id: 8, src: '../assets/cities/madrid.jpg',alt:'Madrid',name:'Madrid', country:'Spain'},
+    {_id: 9, src: '../assets/cities/parislouvre.jpg',alt:'Paris',name:'Paris', country:'France'},
+    {_id: 10, src: '../assets/cities/rome.jpg',alt:'Rome',name:'Rome', country:'Italy'},
+    {_id: 11, src: '../assets/cities/tokyo.jpg',alt:'Tokyo',name:'Tokyo', country:'Japan'},
+    {_id: 12, src: '../assets/cities/newyork.jpg',alt:'New York',name:'New York', country:'United States'},
+    {_id: 13, src: '../assets/cities/moscow.jpg',alt:'Moscow',name:'Moscow', country:'Russia'},
+    {_id: 14, src: '../assets/cities/mexicocity.jpg',alt:'Mexico name',name:'Mexico City', country:'Mexico'},
+    {_id: 15, src: '../assets/cities/stockholm.jpg',alt:'Stockholm',name:'Stockholm', country:'Sweden'},
+]
+
+app.get('/api/cities', (req, res) => {
+    res.json({ response: citiesData })
+})
+
 
 app.listen(4000, () => console.log('Escuchando en el puerto 4000'))

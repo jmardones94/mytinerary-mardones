09c1ff8e5b0281bb0da17be0f1e5d9c9b4a3adf7
jmardones94 <jmardonesg.94@gmail.com>
2021-09-03 15:44:34 -0400

    backend ready to deploy --minor update

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ require("./config/passport")
 
 const app = express()
 const router = require("./routes/index")
-// Middlewares
+
 app.use(cors())
 app.use(express.json())
-
 app.use("/api", router)
 
 app.listen(4000, () => console.log("Server listening on port 4000"))
